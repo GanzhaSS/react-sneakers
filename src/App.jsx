622be3bc6be937480fc75da2a91919bfc055566b
GanzhaSS <ganzhass@nornik.ru>
2023-12-01 17:12:50 +0300

    Need to find error (cart item)

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ const App = () => {
   return (
     <div className="wrapper clear">
       <div className="overlay">
-        <div className="drawer">
-          <h2>Корзина</h2>
+        <div className="drawer  d-flex flex-column">
+          <h2 className="mb-30">Корзина</h2>
+          <div className="Items">
 
-          <div className="cartItem d-flex align-center ">
+          </div>
+          <div className="cartItem d-flex align-center mb-20">
+            <div className="cartItemImg" style={{ backgroundImage: 'url(/img/sneakers/2.jpg)' }}></div>
+            <div className="mr-20">
+              <p className="mb-5">
+                Мужские Кроссовки Nike Air Max 270
+              </p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className="btnRemove" src="/img/icons/btn-remove.svg" alt="Remove" />
+          </div>
+          <div className="cartItem d-flex align-center mb-20">
             {/* <img className="mr-20" width={70} height={70} src="/img/sneakers/2.jpg" alt="Sneakers" /> */}
             <div className="cartItemImg" style={{ backgroundImage: 'url(/img/sneakers/2.jpg)' }}></div>
             <div className="mr-20">
@@ -16,6 +28,19 @@ const App = () => {
             </div>
             <img className="btnRemove" src="/img/icons/btn-remove.svg" alt="Remove" />
           </div>
+
+          <ul className="cartToltalBlock">
+            <li>
+              <span>Итого:</span>
+              <div></div>
+              <b>21 498 руб.</b>
+            </li>
+            <li>
+              <span>Налог 5%:</span>
+              <div></div>
+              <b>1074 руб.</b>
+            </li>
+          </ul>
         </div>
 
       </div>
