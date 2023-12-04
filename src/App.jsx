2dcd335cdae4375541b9ec3cbf63d8a439b0b17e
ganzhass <ganzhass@nornik.ru>
2023-12-04 22:44:24 +0300

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import Header from "./components/Header";
 import Drawer from './components/Drawer';
 
 const arrCards = [
-  { name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999 },
-  { name: 'Мужские Кроссовки Nike Air Max 270', price: 13333 },
-  { name: 'Мужские Кроссовки Nike Air Max 270', price: 14444 }
+  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageURL: "/img/sneakers/1.jpg" },
+  { title: 'Мужские Кроссовки Nike Air Max 270', price: 13333, imageURL: "/img/sneakers/2.jpg" },
+  { title: 'Мужские Кроссовки Nike Air Max 270', price: 14444, imageURL: "/img/sneakers/3.jpg" },
+  { title: 'Мужские Кроссовки Nike Air Max 2705', price: 12222, imageURL: "/img/sneakers/4.jpg" }
 ]
 const App = () => {
   return (
@@ -19,15 +20,14 @@ const App = () => {
           <input placeholder="Поиск ..." />
         </div>
       </div>
-      <div className="d-flex justify-between flex-wrap m-40">
-        <Card />
-        {/* {arrCards.map((obj) => (
-          <Card />
-          // <div>
-          //   <p>{obj.name}</p>
-          //   <p>{obj.price}</p>
-          // </div>
-        ))} */}
+      <div className="d-flex  flex-wrap m-40">
+        {arrCards.map((obj) => (
+          <Card
+            title={obj.title}
+            price={obj.price}
+            imageURL={obj.imageURL} />
+        ))}
+
       </div>
     </div >
   );
